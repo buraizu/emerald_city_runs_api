7d9baabc7ccab8b670b2c89bfa2307fa184481a6
Bryce Eadie <bryce.eadie@gmail.com>
2019-05-06 13:37:24 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :events
+    resources :runs
+
+    resources :users
+    resources :user_events
+    post 'user_token' => 'user_token#create'
+    post 'find_user' => 'users#find'
+    get 'get_events' => 'events#create'
+  end
+
 end
