6deac01b334c2ece3ea290750a5a351083197428
Bryce Eadie <bryce.eadie@gmail.com>
2021-07-20 08:54:29 -0700

    Add trace id correlation for lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -13,8 +13,21 @@ Rails.application.configure do
   
     # This is useful if you want to log query parameters
     config.lograge.custom_options = lambda do |event|
-    { :ddsource => 'ruby',
-      :params => event.payload[:params].reject { |k| %w(controller action).include? k }
-    }
+        # Retrieves trace information for current thread
+        correlation = Datadog.tracer.active_correlation
+    
+        {
+          # Adds IDs as tags to log output
+          :dd => {
+            # To preserve precision during JSON serialization, use strings for large numbers
+            :trace_id => correlation.trace_id.to_s,
+            :span_id => correlation.span_id.to_s,
+            :env => correlation.env.to_s,
+            :service => correlation.service.to_s,
+            :version => correlation.version.to_s
+          },
+          :ddsource => ["ruby"],
+          :params => event.payload[:params].reject { |k| %w(controller action).include? k }
+        }
     end
   end
